4f0b489e62781c042ce1785c80855a1b28c8f262
siladitya21 <siladityadatta@gmail.com>
2022-06-05 01:48:25 +0530

    about me

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, AboutMeComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
