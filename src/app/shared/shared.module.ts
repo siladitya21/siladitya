5061f9bb5bd8e0f207f1ee87383adb130eb4a6f9
siladitya21 <siladityadatta@gmail.com>
2022-06-07 18:46:57 +0530

    majorly done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, SharedRoutingModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent,FooterComponent],
 })
 export class SharedModule {}
