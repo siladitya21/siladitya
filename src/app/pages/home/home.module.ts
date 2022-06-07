b50ac583afa8af0ad084bc81b9b0dfefc43331f8
siladitya21 <siladityadatta@gmail.com>
2022-06-07 16:12:02 +0530

    ss

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,10 +6,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { CvComponent } from './cv/cv.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 
 @NgModule({
-  declarations: [HomePageComponent, AboutMeComponent, CvComponent],
+  declarations: [HomePageComponent, AboutMeComponent, CvComponent, ContactComponent, SkillsComponent, EducationComponent, ExperienceComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
